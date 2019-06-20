3a36a7d1037a8753bf9987f35ff576acaed87c37
Juha Komulainen <juha.komulainen@evident.fi>
2019-06-20 12:25:17 +0300

    Add readResourceLines

--- src/main/kotlin/utils/ResourceUtils.kt
+++ src/main/kotlin/utils/ResourceUtils.kt
@@ -7,3 +7,6 @@ private val classLoader = object {}.javaClass.classLoader
 
 fun openResource(path: String): InputStream =
     classLoader.getResourceAsStream(path) ?: throw IOException("Could not open resource: '$path'")
+
+fun readResourceLines(path: String): List<String> =
+    openResource(path).use { it.reader().readLines() }
